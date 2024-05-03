6751be231de7587b6ce565ece300614c9d3939fd
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-02 12:10:36 -1200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,7 @@
 Contains the class DBStorage
 """
 
-import os 
+import os
 from os import getenv
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
