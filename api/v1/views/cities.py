3265d0ce545254b50ea0640353cbbeffaae2adc6
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-02 12:31:33 -1200

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,57 +1,25 @@
-#!/usr/bin/python3
-"""
-View for City objects that handles default RestFul API actions
-"""
-from flask import jsonify, abort, make_response, request
-from models import storage
-from models.city import City
-from api.v1.views import app_views
-from models.state import State
+#!/usr/bin/python
+"""City class definition"""
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'], strict_slashes=False)
-def handle_cities(state_id):
-    """Handles GET and POST requests for cities under a specific state"""
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
+class City(BaseModel, Base):
+    """Representation of a city"""
 
-    if request.method == 'GET':
-        return jsonify([city.to_dict() for city in state.cities]), 200
-    elif request.method == 'POST':
-        req_data = request.get_json()
-        if not req_data:
-            abort(400, "Not a JSON")
-        if 'name' not in req_data:
-            abort(400, "Missing name")
-        city = City(**req_data)
-        setattr(city, 'state_id', state_id)
-        storage.new(city)
-        storage.save()
-        return make_response(jsonify(city.to_dict()), 201)
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="city")
 
+    else:
+        state_id = ""
+        name = ""
 
-@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
-def city_by_id(city_id):
-    """Handles GET, DELETE, and PUT requests for a specific city"""
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-
-    if request.method == 'GET':
-        return jsonify(city.to_dict()), 200
-    elif request.method == 'DELETE':
-        storage.delete(city)
-        storage.save()
-        return make_response(jsonify({}), 200)
-    elif request.method == 'PUT':
-        req_data = request.get_json()
-        if not req_data:
-            abort(400, "Not a JSON")
-        for key, value in req_data.items():
-            if key not in ['id', 'state_id', 'created_at', 'updated_at']:
-                setattr(city, key, value)
-        storage.save()
-
-        return jsonify({})
-        
+    def __init__(self, *args, **kwargs):
+        """Initialize City instance"""
+        super().__init__(*args, **kwargs)
