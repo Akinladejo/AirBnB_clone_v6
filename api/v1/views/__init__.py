9ecd838e0e8f3bc7d734b9459f87848cf5ba2a4e
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-02 23:33:18 -1200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-""" Initializes the API v1 views """
-
+"""View init Module"""
 from flask import Blueprint
 
-# Import modules without importing their contents
-from api.v1.views import index
-from api.v1.views import states
-from api.v1.views import cities
-from api.v1.views import amenities
-from api.v1.views import users
-from api.v1.views import places
-from api.v1.views import places_reviews
-from api.v1.views import places_amenities
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Create a Blueprint object for API v1 views
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
-
-# Register blueprints from imported modules
-app_views.register_blueprint(index.app_views)
-app_views.register_blueprint(states.app_views)
-app_views.register_blueprint(cities.app_views)
-app_views.register_blueprint(amenities.app_views)
-app_views.register_blueprint(users.app_views)
-app_views.register_blueprint(places.app_views)
-app_views.register_blueprint(places_reviews.app_views)
-app_views.register_blueprint(places_amenities.app_views)
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
