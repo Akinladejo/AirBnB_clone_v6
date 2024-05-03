f428f4546e912a17bd66d63a9bbdea15c5e9ef64
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-02 22:43:14 -1200

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -16,6 +16,7 @@ def get_cities(state_id):
     cities_list = [city.to_dict() for city in state.cities]
     return jsonify(cities_list)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """Retrieves a specific City object by ID"""
@@ -23,6 +24,7 @@ def get_city(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -33,6 +35,7 @@ def delete_city(city_id):
     city.delete()
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create_city(state_id):
@@ -49,6 +52,7 @@ def create_city(state_id):
     city = City(**data)
     city.save()
     return jsonify(city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
