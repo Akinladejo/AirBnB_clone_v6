8f3af7ffc15830831583b37f3ca2554a7596eb4e
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-02 23:20:13 -1200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """index Module"""
-from flask import jsonify
-from api.v1.views import app_views
+from flask import Blueprint, jsonify
 from models import storage
 
+app_views = Blueprint('app_views', __name__)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"}), 200
-
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
