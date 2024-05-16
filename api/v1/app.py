de11ae92a955a8669aa7f614e2296d24790f2316
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-05-16 04:45:40 -1200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,28 @@
- 
 #!/usr/bin/python3
-"""app Module"""
+""" Provide high level structure and rules """
 from flask import Flask, Blueprint, jsonify
+from models import storage
 from api.v1.views import app_views
 from os import getenv
-from models import storage
 from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-host = getenv("HBNB_API_HOST") or '0.0.0.0'
-port = getenv("HBNB_API_PORT") or 5000
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def close_storage(self):
-    """Calls storage.close()"""
+def teardown(self):
+    """ teardown the app context """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Returns a JSON-formatted 404 status code response"""
-    return jsonify({'error': 'Not found'}), 404
+def error_404(error):
+    """ return a json 404 error """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
